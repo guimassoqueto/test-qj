b53acb2d2aa4f228e7b834128520640bfd1f325e
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-07 11:15:48 -0300

    feat: update generate_sql_output function

--- main.py
+++ main.py
@@ -61,17 +61,19 @@ def generate_sql_output(
     with open(csv_output_file, "r", encoding="utf-8") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         header_row = csv_reader.fieldnames
-        csv_reader_length = len(csv_reader)
+        csv_reader = list(csv_reader)
 
-        f = open(sql_output_file_name, "a")
+        f = open(sql_output_file_name, "w")
         f.write("INSERT INTO dados_finais")
         f.write(f'({", ".join(header_row)})')
         f.write(" VALUES ")
         
+        csv_reader_length = len(csv_reader)
         for i, row in enumerate(csv_reader):
             line = [f"'{row[key]}'" for key in header_row]
-            if (i + 1 == csv_reader_length): f.write(f'({", ".join(line)})\n')
+            if (i+1 == csv_reader_length): f.write(f'({", ".join(line)})\n')
             else: f.write(f'({", ".join(line)}),\n')
+            
             
         f.write(";")
         f.close()
